39a2c2a80c9df67b53616ef7acbdcb5fb475151f
MrILL <ilua2403@ukr.net>
2020-11-01 21:22:21 +0200

    qfix

--- handler.go
+++ handler.go
@@ -1,18 +1,16 @@
 package lab2
 
-
 import (
 	"fmt"
 	"io"
 	"io/ioutil"
-	. "github.com/FictProger/architecture-lab-2"
 )
+
 // ComputeHandler should be constructed with input io.Reader and output io.Writer.
 // Its Compute() method should read the expression from input and write the computed result to the output.
 type ComputeHandler struct {
-	Input io.Reader
+	Input  io.Reader
 	Output io.Writer
-
 }
 
 func (ch *ComputeHandler) Compute() error {
